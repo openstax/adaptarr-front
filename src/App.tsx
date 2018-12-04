72f2ebb18b0aadada67c240be543108aa504756a
Piotr Kozlowski <pior@obrandowani.pl>
2018-12-04 15:13:56 +0100

    Fetch and create booksMap and modulesMap

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ import Settings from './screens/app/Settings'
 
 import * as userActions from './store/actions/User'
 import * as notificationsActions from './store/actions/Notifications'
+import * as booksActions from './store/actions/Books'
+import * as modulesActions from './store/actions/Modules'
+import * as types from './store/types'
 import { State } from './store/reducers/index'
 
 import './assets/styles/shared.css'
@@ -20,21 +23,33 @@ import './assets/styles/shared.css'
 type Props = {
   user: {}
   notifications: {}
+  booksMap: {
+    booksMap: types.BooksMap
+  }
+  modulesMap: {
+    modulesMap: types.ModulesMap
+  }
   fetchUser: () => void
   fetchNotifications: () => void
+  fetchBooksMap: () => void
+  fetchModulesMap: () => void
 }
 
-export const mapStateToProps = ({ user, notifications }: State) => {
+export const mapStateToProps = ({ user, notifications, booksMap, modulesMap }: State) => {
   return {
     user,
     notifications,
+    booksMap,
+    modulesMap,
   }
 }
 
-export const mapDispatchToProps = (dispatch: userActions.FetchUser | notificationsActions.FetchNotifications) => {
+export const mapDispatchToProps = (dispatch: userActions.FetchUser | notificationsActions.FetchNotifications | booksActions.FetchBooksMap | modulesActions.FetchModulesMap) => {
   return {
     fetchUser: () => dispatch(userActions.fetchUser()),
     fetchNotifications: () => dispatch(notificationsActions.fetchNotifications()),
+    fetchBooksMap: () => dispatch(booksActions.fetchBooksMap()),
+    fetchModulesMap: () => dispatch(modulesActions.fetchModulesMap()),
   }
 }
 
@@ -43,6 +58,8 @@ class App extends React.Component<Props> {
   componentDidMount () {
     this.props.fetchUser()
     this.props.fetchNotifications()
+    this.props.fetchBooksMap()
+    this.props.fetchModulesMap()
   }
 
   public render() {
