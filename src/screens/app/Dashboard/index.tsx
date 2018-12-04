b6de4908dcee2b019ca61ea7a93e4774e6d48da9
Piotr Kozlowski <pior@obrandowani.pl>
2018-12-04 15:34:05 +0100

    Display drafts and modules assigned to user

--- src/screens/app/Dashboard/index.tsx
+++ src/screens/app/Dashboard/index.tsx
@@ -2,18 +2,30 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 
 import Header from '../../../components/Header'
+import Loader from '../../../components/Loader'
 
 import * as dashboardActions from '../../../store/actions/Dashboard'
+import * as types from '../../../store/types'
 import { State } from '../../../store/reducers/index'
 
 type Props = {
-  dashboard: {}
+  dashboard: {
+    dashboard: types.Dashboard
+  }
+  booksMap: {
+    booksMap: types.BooksMap
+  }
+  modulesMap: {
+    modulesMap: types.ModulesMap
+  }
   fetchDashboard: () => void
 }
 
-export const mapStateToProps = ({ dashboard }: State) => {
+export const mapStateToProps = ({ dashboard, booksMap, modulesMap }: State) => {
   return {
     dashboard,
+    booksMap,
+    modulesMap,
   }
 }
 
@@ -23,8 +35,53 @@ export const mapDispatchToProps = (dispatch: dashboardActions.FetchDashboard) =>
   }
 }
 
-class Dashboard extends React.Component<Props> {
+class Dashboard extends React.Component<Props, any, any> {
+
+  private booksMap = this.props.booksMap.booksMap
+  private modulesMap = this.props.modulesMap.modulesMap
   
+  private isLoading = this.props.dashboard.dashboard.isLoading
+  private drafts = this.props.dashboard.dashboard.drafts
+  private assigned = this.props.dashboard.dashboard.assigned
+
+  listOfDrafts (arr: types.DashboardDraft[]) {
+    if (arr.length > 0) {
+      return (
+        <ul className="list list--drafts">
+          {
+            arr.map(draftId => {
+              const currModule = this.modulesMap.get(draftId)
+              const title = currModule ? currModule.title : 'undefined'
+              
+              return <li key={draftId} className="list__item">{title}</li>
+            })
+          }
+        </ul>
+      )
+    } else {
+      return 'You don\'t have any drafts.'
+    }
+  }
+
+  listOfAssigned (arr: types.DashboardAssignedModule[]) {
+    if (arr.length > 0) {
+      return (
+        <ul className="list list--assigned">
+          {
+            arr.map(el => {
+              const currModule = this.modulesMap.get(el.id)
+              const title = currModule ? currModule.title : 'undefined'
+
+              return <li key={el.id} className="list__item">{title}</li>
+            })
+          }
+        </ul>
+      )
+    } else {
+      return 'You are not assigned to any module.'
+    }
+  }
+
   componentDidMount () {
     this.props.fetchDashboard()
   }
@@ -34,7 +91,20 @@ class Dashboard extends React.Component<Props> {
       <section className="section--wrapper">
         <Header title={"Dashboard"} />
         <div className="section__content">
-          Dashboard data: { JSON.stringify(this.props.dashboard) }
+          <Loader isLoading={this.isLoading}>
+            <div className="section__half">
+              <h3 className="section__heading">Your drafts:</h3>
+              {
+                this.listOfDrafts(this.drafts)
+              }
+            </div>
+            <div className="section__half">
+              <h3 className="section__heading">Assigned to you:</h3>
+              {
+                this.listOfAssigned(this.assigned)
+              }
+            </div>
+          </Loader>
         </div>
       </section>
     )
