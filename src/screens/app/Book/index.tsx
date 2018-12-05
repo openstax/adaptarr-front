7182efdafd47a87779926f5b9b0da010bc328345
Piotr Kozlowski <pior@obrandowani.pl>
2018-12-05 13:44:36 +0100

    Initial component for Book screen

--- src/screens/app/Book/index.tsx
+++ src/screens/app/Book/index.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
+import { AxiosResponse } from 'axios'
 
+import axios from '../../../config/axios'
+
+import Section from '../../../components/Section'
 import Header from '../../../components/Header'
+
+import * as types from '../../../store/types'
 
 type Props = {
   match: {
@@ -10,17 +16,49 @@ type Props = {
   }
 }
 
-const book = (props: Props) => {
-  const bookTitle = 'Loading...'
+class Book extends React.Component<Props> {
+  state: {
+    isLoading: boolean,
+    book: types.Book
+  } = {
+    isLoading: true,
+    book: {
+      id: 'loading',
+      title: 'Loading...',
+      parts: [],
+    },
+  }
+
+  private fetchBook = (id: string) => {
+    axios.get(`books/${id}`)
+      .then((res: AxiosResponse) => {
+        this.setState({
+          isLoading: false,
+          book: res.data,
+        })
+      })
+      .catch((e: ErrorEvent) => {
+        console.log(e.message)
+        this.setState({isLoading: false})
+      })
+  }
+
+  componentDidMount () {
+    this.fetchBook(this.props.match.params.id)
+  }
   
-  return (
-    <section className="section--wrapper">
-      <Header title={bookTitle} />
-      <div className="section__content">
-        Book
-      </div>
-    </section>
-  )
+  public render() {
+    const { book } = this.state
+
+    return (
+      <Section>
+        <Header title={book.title} />
+        <div className="section__content">
+          {JSON.stringify(book.parts)}
+        </div>
+      </Section>
+    )
+  }
 }
 
-export default book
+export default Book
