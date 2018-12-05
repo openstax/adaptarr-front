69b9631fff8634ca24b9f923ebc34eec6202611a
Piotr Kozlowski <pior@obrandowani.pl>
2018-12-05 09:15:42 +0100

    Add "to" prop to Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,22 +1,32 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
+import { LocationDescriptor } from 'history'
 
 type Props = {
   color?: 'green' | 'red'
   size?: 'small' | 'medium' | 'big'
   children: React.ReactNode
   clickHandler?: any
+  to?: LocationDescriptor
 }
 
-const button = ({ color, size, children, clickHandler }: Props) => {
+const button = ({ color, size, children, clickHandler, to }: Props) => {
   const classes: string[] = ['button']
 
   if (color) classes.push(`button--${color}`)
   if (size) classes.push(`button--${size}`)
 
   return (
-    <button className={classes.join(' ')} onClick={clickHandler}>
-      {children}
-    </button>
+    <React.Fragment>
+      {
+        to ?
+          <Link to={to} className={classes.join(' ')}>{children}</Link>
+        :
+          <button className={classes.join(' ')} onClick={clickHandler}>
+            {children}
+          </button>
+      }
+    </React.Fragment>
   )
 }
 
