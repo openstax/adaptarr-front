0656895558ba575398c6d60d5997db76a5fd7f13
Piotr Kozlowski <pior@obrandowani.pl>
2018-12-06 16:36:21 +0100

    Use react-nestable module with onMove() functionality and update types

--- src/@types/react-nestable/index.d.ts
+++ src/@types/react-nestable/index.d.ts
@@ -1,16 +1,24 @@
 declare module 'react-nestable' {
   import * as React from 'react'
 
+  export interface Item {
+    id: string | number
+    [key: string]: any
+  }
+
+  export type RenderItem = {item: {}, index: number, collapseIcon: any, handler: any}
+
   export interface NestableProps {
-    items: any[]
+    items: Item[]
     threshold?: number
     maxDepth?: number
     collapsed?: boolean
     group?: string | number
     handler?: React.Component
     childrenProp?: string
-    renderItem?: any//({item: any, index?: number})
+    renderItem: (payload: RenderItem) => any
     renderCollapseIcon?: any//({isCollapsed: boolean})
+    onMove?: () => boolean
     onChange?: any//(items: [], item: any)
   }
 
