a065e58a11cd30c8a7662bc96e0ef0ee70a7ac58
Piotr Kozlowski <pior@obrandowani.pl>
2018-12-07 12:15:51 +0100

    Render Avatar components for users assigned to the modules

--- src/screens/app/Book/index.tsx
+++ src/screens/app/Book/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import Nestable from 'react-nestable'
+import { connect } from 'react-redux'
 import { AxiosResponse } from 'axios'
 
 import axios from '../../../config/axios'
 
 import Section from '../../../components/Section'
 import Header from '../../../components/Header'
+import Avatar from '../../../components/ui/Avatar'
 
 import * as types from '../../../store/types'
+import { State } from '../../../store/reducers'
 
 type Props = {
   match: {
@@ -15,22 +18,19 @@ type Props = {
       id: string
     }
   }
+  team: {
+    teamMap: types.TeamMap
+  }
+  modulesMap: {
+    modulesMap: types.ModulesMap
+  }
 }
 
-const renderItem = ({ item, collapseIcon }: { item: types.BookPart, index: number, collapseIcon: any, handler: any }) => {
-  return (
-    <div className="bookpart__item">
-      <span className="bookpart__icon">
-        {collapseIcon}
-      </span>
-      {
-        item.kind == 'part' ?
-          <h2 className="bookpart__title bookpart__title--chapter">{item.title}</h2>
-        :
-          <h3 className="bookpart__title bookpart__title--module">{item.title}</h3>
-      }
-    </div>
-  )
+const mapStateToProps = ({ team, modulesMap }: State) => {
+  return {
+    team,
+    modulesMap,
+  }
 }
 
 // Nested component is adding `parts: []` to every item and we do not want that
@@ -66,58 +66,39 @@ class Book extends React.Component<Props> {
     book: {
       id: 'loading',
       title: 'Loading...',
-      parts: [
-        {
-          "kind":"module",
-          "id":"66df3c1s4-44f8-4a0c-8a44-b4671f6a08a7",
-          "title":"1. Rise and Fall of Jabba the Hutt"
-        },
-        {
-          "kind":"part",
-          "id":"6f0e5297-e64e-43fdfga-b434-4ef26ab3766a",
-          "title":"2. Notable Figures",
-          "parts":[
-            {
-              "kind":"module",
-              "id":"23310344-6bb2-470b-a643-8bb04125e2e5",
-              "title":"2.1. The Skywalkers"
-            },
-            {
-              "kind":"module",
-              "id":"233120344-6bb2-470b-a643-8bb04125e2e5",
-              "title":"2.2. The Skywalkers"
-            },
-            {
-              "kind":"module",
-              "id":"233103344-6bb2-470b-a643-8bb04125e2e5",
-              "title":"2.3. The Skywalkers"
-            }
-          ]
-        },
-        {
-          "kind":"part",
-          "id":"6f0e529as7-e64e-43fa-b434-4ef26ab3766a",
-          "title":"3. Notable Figures",
-          "parts":[
-            {
-              "kind":"module",
-              "id":"23310344-6b4b2-470b-a643-8bb04125e2e5",
-              "title":"3.1. The Skywalkers"
-            },
-            {
-              "kind":"module",
-              "id":"233120344-6bb32-470b-a643-8bb04125e2e5",
-              "title":"3.2. The Skywalkers"
-            },
-            {
-              "kind":"module",
-              "id":"233103344-6b2b2-470b-a643-8bb04125e2e5",
-              "title":"3.3. The Skywalkers"
-            }
-          ]
-        }
-      ],
+      parts: []
     },
+  }
+
+  private renderItem = ({ item, collapseIcon }: { item: types.BookPart, index: number, collapseIcon: any, handler: any }) => {
+    const { modulesMap } = this.props.modulesMap
+    const { teamMap } = this.props.team
+    let user
+
+    if (item.kind === 'module') {
+      const mod = modulesMap.get(item.id)
+      const assigne = mod ? mod.assignee : undefined
+      user = assigne ? teamMap.get(assigne) : undefined
+    }
+
+    let classes = ['bookpart__item', `bookpart__item--${item.kind}`]
+    return (
+      <div className={classes.join(' ')}>
+        {
+          item.kind === 'part' ?
+            <span className="bookpart__icon">
+              {collapseIcon}
+            </span>
+          : null
+        }
+        <span className="bookpart__title">{item.title}</span>
+        {
+          item.kind === 'module' && user ?
+            <Avatar size="small" user={user} />
+          : null
+        }
+      </div>
+    )
   }
 
   private findParentWithinItems = (items: types.BookPart[], path: number[]) => {
@@ -191,24 +172,27 @@ class Book extends React.Component<Props> {
     const { book } = this.state
 
     return (
-      <Section>
-        <Header title={book.title} />
-        <div className="section__content">
-          {/*<div style={{width: '100%'}}>
-            <pre>{JSON.stringify(book.parts, undefined, 2)}</pre>
-          </div>*/}
-          <Nestable
-            isDisabled={false}
-            items={book.parts}
-            childrenProp="parts"
-            renderItem={renderItem}
-            onMove={this.handleOnMove}
-            onChange={this.handlePositionChange}
-          />
-        </div>
-      </Section>
+      <div className="container container--splitted">
+        <Section>
+          <Header title={book.title} />
+          <div className="section__content">
+            {/*<div style={{width: '100%'}}>
+              <pre>{JSON.stringify(book.parts, undefined, 2)}</pre>
+            </div>*/}
+            <Nestable
+              isDisabled={false}
+              items={book.parts}
+              className="book-collection"
+              childrenProp="parts"
+              renderItem={this.renderItem}
+              onMove={this.handleOnMove}
+              onChange={this.handlePositionChange}
+            />
+          </div>
+        </Section>
+      </div>
     )
   }
 }
 
-export default Book
+export default connect(mapStateToProps)(Book)
