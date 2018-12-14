0181c0cc23110aa84bdca1e846b133273448989a
Piotr Kozlowski <pior@obrandowani.pl>
2018-12-14 09:24:23 +0100

    Allow creating new modules in ModulesList container

--- src/containers/ModulesList/index.tsx
+++ src/containers/ModulesList/index.tsx
@@ -2,18 +2,25 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { Trans } from 'react-i18next'
 
-import Button from '../../components/ui/Button'
-import ModuleInfo from '../../components/ModuleInfo'
-import Icon from '../../components/ui/Icon'
+import axios from 'src/config/axios'
 
-import { ModulesMap, ModuleShortInfo } from '../../store/types'
-import { State } from '../../store/reducers'
+import Button from 'src/components/ui/Button'
+import ModuleInfo from 'src/components/ModuleInfo'
+import Icon from 'src/components/ui/Icon'
+
+import { ModulesMap, ModuleShortInfo } from 'src/store/types'
+import { State } from 'src/store/reducers'
+import AdminUI from 'src/components/AdminUI'
+import SuperSession from 'src/components/SuperSession'
+
+import { FetchModulesMap, fetchModulesMap } from 'src/store/actions/Modules'
 
 type Props = {
   modulesMap: {
     modulesMap: ModulesMap
   }
   onModuleClick: (mod: ModuleShortInfo) => any
+  fetchModulesMap: () => void
 }
 
 const mapStateToProps = ({ modulesMap }: State) => {
@@ -22,7 +29,21 @@ const mapStateToProps = ({ modulesMap }: State) => {
   }
 }
 
+const mapDispatchToProps = (dispatch: FetchModulesMap) => {
+  return {
+    fetchModulesMap: () => dispatch(fetchModulesMap()),
+  }
+}
+
 class ModuleList extends React.Component<Props> {
+
+  state: {
+    moduleTitleValue: string
+    showSuperSession: boolean
+  } = {
+    moduleTitleValue: '',
+    showSuperSession: false,
+  }
 
   private listOfModules = (modulesMap: ModulesMap) => {
     let modules: ModuleShortInfo[] = []
@@ -45,18 +66,64 @@ class ModuleList extends React.Component<Props> {
     this.props.onModuleClick(mod)
   }
 
+  private updateModuleTitleValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ moduleTitleValue: e.target.value })
+  }
+
+  private addNewModule = () => {
+    axios.post('modules', {title: this.state.moduleTitleValue})
+      .then(res => {
+        this.props.onModuleClick(res.data)
+        this.props.fetchModulesMap()
+      })
+      .catch(e => {
+        if (e.request.status === 403) {
+          this.setState({ showSuperSession: true })
+        } else {
+          console.error(e.message)
+        }
+      })
+  }
+
+  private superSessionSuccess = () => {
+    this.addNewModule()
+    this.setState({ showSuperSession: false })
+  }
+
+  private superSessionFailure = (e: Error) => {
+    console.log('failure', e.message)
+  }
+
   public render() {
+    const { moduleTitleValue, showSuperSession } = this.state
     const modulesMap = this.props.modulesMap.modulesMap
 
     return (
       <div className="modulesList">
-        <div className="modulesList__new">
-          <input type="text" placeholder="Title" />
-          <Button>
-            <Icon name="plus" />
-            <Trans i18nKey="Buttons.addNew" />
-          </Button>
-        </div>
+        {
+          showSuperSession ?
+            <SuperSession
+              onSuccess={this.superSessionSuccess} 
+              onFailure={this.superSessionFailure}
+              onAbort={() => this.setState({ showSuperSession: false })}/>
+          : null
+        }
+        <AdminUI>
+          <div className="modulesList__new">
+            <input 
+              type="text" 
+              placeholder="Title"
+              value={moduleTitleValue}
+              onChange={(e) => this.updateModuleTitleValue(e)} />
+            <Button 
+              isDisabled={moduleTitleValue.length === 0}
+              clickHandler={this.addNewModule}
+            >
+              <Icon name="plus" />
+              <Trans i18nKey="Buttons.addNew" />
+            </Button>
+          </div>
+        </AdminUI>
         {
           modulesMap.size > 0 ?
             <ul className="modulesList__list">
@@ -69,4 +136,4 @@ class ModuleList extends React.Component<Props> {
   }
 }
 
-export default connect(mapStateToProps)(ModuleList)
+export default connect(mapStateToProps, mapDispatchToProps)(ModuleList)
