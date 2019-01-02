84d37572604108a73967e8048181f8d198698200
Piotr Kozlowski <pior@obrandowani.pl>
2019-01-02 09:50:27 +0100

    Add new helper to sort arrays by title prop

---
+++ src/helpers/sortArrayByTitle.tsx
@@ -0,0 +1,15 @@
+type Obj = {
+  title: string
+}
+
+const sortArrayByTitle = (a: Obj, b: Obj): number => {
+  if (a.title > b.title) {
+    return 1
+  } else if (a.title < b.title) {
+    return -1
+  } else {
+    return 0
+  }
+}
+
+export default sortArrayByTitle
