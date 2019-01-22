dd707d59abea87cbd57450fac372e001215031ad
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-01-22 09:12:30 +0100

    Add Modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import { Portal } from 'react-portal'
+
+import './index.css'
+
+export type Props = {
+  target?: string,
+  content?: () => React.ReactNode,
+  onClose?: () => void,
+}
+
+export default class Modal extends React.Component<Props> {
+  state: {
+    open: boolean,
+  } = {
+    open: false,
+  }
+
+  back: HTMLElement | null = null
+
+  /**
+   * Open this modal.
+   */
+  open() {
+    this.setState({ open: true })
+  }
+
+  /**
+   * Close this modal.
+   */
+  close() {
+    this.setState({ open: false })
+  }
+
+  render() {
+    if (!this.state.open) return null
+
+    const { content, children, target } = this.props
+    const c = content ? content() : children
+
+    return <Portal node={document.getElementById(target || 'modal-root')}>
+      <div
+        className="modal"
+        tabIndex={0}
+        onClick={this.onClick}
+        onKeyDown={this.onKeyDown}
+        ref={el => el && (this.back = el)}
+        >
+        {c}
+      </div>
+    </Portal>
+  }
+
+  onClick = (ev: React.MouseEvent) => {
+    if (ev.target === this.back) {
+      this.onClose()
+    }
+  }
+
+  onKeyDown = (ev: React.KeyboardEvent) => {
+    if (ev.key === 'Escape') {
+      this.onClose()
+    }
+  }
+
+  private onClose() {
+    this.close()
+
+    const { onClose } = this.props
+    if (onClose) onClose()
+  }
+}
