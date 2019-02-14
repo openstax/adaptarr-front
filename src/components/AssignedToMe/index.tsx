f0aecb97f99b9efafab837296ee4db5737329dd3
Piotr Kozlowski <pior@obrandowani.pl>
2019-02-14 09:15:39 +0100

    Temporary solution for sorting drafts and modules by books names

--- src/components/AssignedToMe/index.tsx
+++ src/components/AssignedToMe/index.tsx
@@ -1,66 +1,148 @@
+import './index.css'
+
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { Trans } from 'react-i18next'
 
 import * as api from 'src/api'
+import sortArrayByTitle from 'src/helpers/sortArrayByTitle'
+import { State } from 'src/store/reducers'
+import { BooksMap } from 'src/store/types'
 
 import Button from 'src/components/ui/Button'
-
-import { State } from 'src/store/reducers'
 
 type Props = {
   modules: {
     assignedToMe: api.Module[]
   }
+  booksMap: {
+    booksMap: BooksMap
+  }
   drafts: api.Draft[]
   onCreateDraft: (mod: api.Module) => any
 }
 
-const mapStateToProps = ({ modules }: State) => {
+type BooksWithModules = {
+  books: api.Book[]
+  modules: api.Module[]
+}
+
+const mapStateToProps = ({ modules, booksMap }: State) => {
   return {
     modules,
+    booksMap,
   }
 }
 
-const assignedToMe = (props: Props) => {
-  return (
-    <div className="draftsList">
-      {
-        props.modules.assignedToMe.length ?
-          <ul className="list draftsList__list">
-            {
-              props.modules.assignedToMe.map(mod => {
-                return (
-                  <li key={mod.id} className="list__item">
-                    <span className="list__title">
-                      {mod.title}
-                    </span>
-                    <span className="list__buttons">
-                      {
-                        props.drafts.some(draft => draft.module === mod.id) ?
-                          <Button 
-                            to={`/modules/${mod.id}`}
-                          >
-                            <Trans i18nKey="Buttons.viewDraft" />
-                          </Button>
-                        :
-                          <Button
-                            color="green"
-                            clickHandler={() => props.onCreateDraft(mod)}
-                          >
-                            <Trans i18nKey="Buttons.newDraft" />
-                          </Button>
-                      }
-                    </span>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        : <Trans i18nKey="AssignedToMe.noAssigned"/>
+class AssignedToMe extends React.Component<Props> {
+
+  state: {
+    booksWithModules: Map<string, BooksWithModules> | undefined
+  } = {
+    booksWithModules: undefined,
+  }
+
+  private sortModulesByBooksName = async () => {
+    const { modules: { assignedToMe }, booksMap: { booksMap } } = this.props
+
+    let modulesByBooks: Map<string, BooksWithModules> = new Map()
+
+    for (const mod of assignedToMe) {
+      const booksIds = await mod.books()
+      const books = booksIds.map(bookId => booksMap.get(bookId)!)
+
+      let booksName = books.length ? '' : 'Not assigned to any book'
+      books.forEach((b, i) => {
+        if (i === books.length - 1) {
+          booksName += b.title
+        } else {
+          booksName += b.title + ', '
+        }
+      })
+
+      const booksWithModules = modulesByBooks.get(booksName)
+
+      if (booksWithModules) {
+        // Update modules list for this books
+        let mods = booksWithModules.modules
+        if (!mods.some(m => m.id === mod.id)) {
+          // If this modules is not on the list for this books then add it
+          mods.push(mod)
+        }
+        modulesByBooks.set(booksName, {books: booksWithModules.books, modules: mods.sort(sortArrayByTitle)})
+      } else {
+        // Create entry for this books
+        modulesByBooks.set(booksName, {books, modules: [mod]})
       }
-    </div>
-  )
+    }
+
+    this.setState({ booksWithModules: modulesByBooks })
+  }
+
+  componentDidUpdate = (prevProps: Props) => {
+    if (prevProps.modules.assignedToMe !== this.props.modules.assignedToMe) {
+      this.sortModulesByBooksName()
+    }
+  }
+
+  componentDidMount = () => {
+    this.sortModulesByBooksName()
+  }
+  
+  public render() {
+    const { booksWithModules } = this.state
+    const { drafts } = this.props
+
+    return (
+      <div className="modulesList">
+        {
+          booksWithModules ?
+            <ul className="list">
+              {
+                Array.from(booksWithModules.entries()).map(([booksName, data]) => (
+                  <li key={booksName} className="list__item modulesList__book">
+                    <strong>{booksName}</strong>
+                    {
+                      data.modules.length ?
+                        <ul className="list">
+                          {
+                            data.modules.map(mod => (
+                              <li key={mod.id} className="list__item">
+                                <span className="list__title">
+                                  {mod.title}
+                                </span>
+                                <span className="list__buttons">
+                                  {
+                                   drafts.some(draft => draft.module === mod.id) ?
+                                      <Button 
+                                        to={`/modules/${mod.id}`}
+                                      >
+                                        <Trans i18nKey="Buttons.viewDraft" />
+                                      </Button>
+                                    :
+                                      <Button
+                                        color="green"
+                                        clickHandler={() => this.props.onCreateDraft(mod)}
+                                      >
+                                        <Trans i18nKey="Buttons.newDraft" />
+                                      </Button>
+                                  }
+                                </span>
+                              </li>
+                            ))
+                          }
+                        </ul>
+                      : null
+                    }
+                  </li>
+                ))
+              }
+            </ul>
+          : <Trans i18nKey="AssignedToMe.noAssigned"/>
+        }
+      </div>
+    )
+  }
 }
 
-export default connect(mapStateToProps)(assignedToMe)
+export default connect(mapStateToProps)(AssignedToMe)
--- src/components/DraftsList/index.tsx
+++ src/components/DraftsList/index.tsx
@@ -1,50 +1,139 @@
+import './index.css'
+
 import * as React from 'react'
+import { connect } from 'react-redux'
 import { Trans } from 'react-i18next'
 
 import * as api from 'src/api'
+import sortArrayByTitle from 'src/helpers/sortArrayByTitle'
+import { State } from 'src/store/reducers'
+import { BooksMap } from 'src/store/types'
 
 import Button from 'src/components/ui/Button'
 
 type Props = {
+  booksMap: {
+    booksMap: BooksMap
+  }
   drafts: api.Draft[]
   onDraftDeleteClick: (draft: api.Draft) => any
 }
 
-const draftsList = (props: Props) => {
-  return (
-    <div className="draftsList">
-      {
-        props.drafts.length ?
-          <ul className="list draftsList__list">
-            {
-              props.drafts.map(draft => {
-                return (
-                  <li key={draft.module} className="list__item">
-                    <span className="list__title">
-                      {draft.title}
-                    </span>
-                    <span className="list__buttons">
-                      <Button 
-                        to={`/drafts/${draft.module}`}
-                      >
-                        <Trans i18nKey="Buttons.viewDraft" />
-                      </Button>
-                      <Button
-                        color="red"
-                        clickHandler={() => props.onDraftDeleteClick(draft)}
-                      >
-                        <Trans i18nKey="Buttons.delete" />
-                      </Button>
-                    </span>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        : <Trans i18nKey="DraftsList.noDraftsFound"/>
-      }
-    </div>
-  )
+type BooksWithDrafts = {
+  books: api.Book[]
+  drafts: api.Draft[]
 }
 
-export default draftsList
+const mapStateToProps = ({ booksMap }: State) => {
+  return {
+    booksMap,
+  }
+}
+
+class DraftsList extends React.Component<Props> {
+
+  state: {
+    booksWithDrafts: Map<string, BooksWithDrafts> | undefined
+  } = {
+    booksWithDrafts: undefined,
+  }
+
+  private sortDraftsByBooksName = async () => {
+    const { drafts, booksMap: { booksMap } } = this.props
+
+    let draftsByBooks: Map<string, BooksWithDrafts> = new Map()
+
+    for (const draft of drafts) {
+      const booksIds = await draft.books()
+      const books = booksIds.map(bookId => booksMap.get(bookId)!)
+
+      let booksName = books.length ? '' : 'Not assigned to any book'
+      books.forEach((b, i) => {
+        if (i === books.length - 1) {
+          booksName += b.title
+        } else {
+          booksName += b.title + ', '
+        }
+      })
+
+      const booksWithDrafts = draftsByBooks.get(booksName)
+
+      if (booksWithDrafts) {
+        // Update drafts list for this books
+        let drfts = booksWithDrafts.drafts
+        if (!drfts.some(d => d.module === draft.module)) {
+          // If this draft is not on the list for this books then add it
+          drfts.push(draft)
+        }
+        draftsByBooks.set(booksName, {books: booksWithDrafts.books, drafts: drfts.sort(sortArrayByTitle)})
+      } else {
+        // Create entry for this books
+        draftsByBooks.set(booksName, {books, drafts: [draft]})
+      }
+    }
+
+    this.setState({ booksWithDrafts: draftsByBooks })
+  }
+
+  componentDidUpdate = (prevProps: Props) => {
+    if (prevProps.drafts !== this.props.drafts) {
+      this.sortDraftsByBooksName()
+    }
+  }
+
+  componentDidMount = () => {
+    this.sortDraftsByBooksName()
+  }
+  
+  public render() {
+    const { booksWithDrafts } = this.state
+
+    return (
+      <div className="draftsList">
+        {
+          booksWithDrafts ?
+            <ul className="list">
+              {
+                Array.from(booksWithDrafts.entries()).map(([booksName, data]) => (
+                  <li key={booksName} className="list__item draftsList__book">
+                    <strong>{booksName}</strong>
+                    {
+                      data.drafts.length ?
+                        <ul className="list">
+                          {
+                            data.drafts.map(draft => (
+                              <li key={draft.module} className="list__item">
+                                <span className="list__title">
+                                  {draft.title}
+                                </span>
+                                <span className="list__buttons">
+                                <Button 
+                                  to={`/drafts/${draft.module}`}
+                                >
+                                  <Trans i18nKey="Buttons.viewDraft" />
+                                </Button>
+                                <Button
+                                  color="red"
+                                  clickHandler={() => this.props.onDraftDeleteClick(draft)}
+                                >
+                                  <Trans i18nKey="Buttons.delete" />
+                                </Button>
+                              </span>
+                              </li>
+                            ))
+                          }
+                        </ul>
+                      : null
+                    }
+                  </li>
+                ))
+              }
+            </ul>
+          : <Trans i18nKey="DraftsList.noDraftsFound"/>
+        }
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(DraftsList)
