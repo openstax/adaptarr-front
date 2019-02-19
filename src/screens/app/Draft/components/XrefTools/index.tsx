917408e985712b9e9cf09dce17fe463b780206c1
Piotr Kozlowski <pior@obrandowani.pl>
2019-02-19 09:33:08 +0100

    Show cases depend on selected language

--- src/screens/app/Draft/components/XrefTools/index.tsx
+++ src/screens/app/Draft/components/XrefTools/index.tsx
@@ -4,6 +4,7 @@ import { Trans } from 'react-i18next'
 import { Editor, Value } from 'slate'
 
 import i18n from 'src/i18n'
+import getCurrentLng from 'src/helpers/getCurrentLng'
 import { Module } from 'src/api'
 import { ReferenceTarget } from 'src/store/types'
 
@@ -16,15 +17,25 @@ import XrefTargetSelector from 'src/containers/XrefTargetSelector'
 
 type SelectOption = { value: string, label: string }
 
-const CASES: SelectOption[] = [
-  { value: 'none', label: i18n.t('Editor.xref.cases.none') },
-  { value: 'genitive', label: i18n.t('Editor.xref.cases.genitive') },
-  { value: 'dative', label: i18n.t('Editor.xref.cases.dative') },
-  { value: 'accusative', label: i18n.t('Editor.xref.cases.accusative') },
-  { value: 'instrumental', label: i18n.t('Editor.xref.cases.instrumental') },
-  { value: 'locative', label: i18n.t('Editor.xref.cases.locative') },
-  { value: 'vocative', label: i18n.t('Editor.xref.cases.vocative') },
+const LANGUAGES: SelectOption[] = [
+  { value: 'en', label: 'English' },
+  { value: 'pl', label: 'Polski' },
 ]
+
+const CASES: { [key: string]: SelectOption[] } = {
+  en: [
+    { value: 'none', label: i18n.t('Editor.xref.cases.none') },
+  ],
+  pl: [
+    { value: 'none', label: i18n.t('Editor.xref.cases.none') },
+    { value: 'genitive', label: i18n.t('Editor.xref.cases.genitive') },
+    { value: 'dative', label: i18n.t('Editor.xref.cases.dative') },
+    { value: 'accusative', label: i18n.t('Editor.xref.cases.accusative') },
+    { value: 'instrumental', label: i18n.t('Editor.xref.cases.instrumental') },
+    { value: 'locative', label: i18n.t('Editor.xref.cases.locative') },
+    { value: 'vocative', label: i18n.t('Editor.xref.cases.vocative') },
+  ],
+}
 
 export type Props = {
   editor: Editor,
@@ -32,6 +43,12 @@ export type Props = {
 }
 
 export default class XrefTools extends React.Component<Props> {
+  state: {
+    language: SelectOption
+  } = {
+    language: LANGUAGES.find(lng => lng.value === getCurrentLng('iso')) || LANGUAGES[0]
+  }
+
   xrefModal: Modal | null = null
 
   public render() {
@@ -39,14 +56,28 @@ export default class XrefTools extends React.Component<Props> {
 
     if (!xref) return null
 
+    const { language } = this.state
+
     return (
       <ToolGroup title="Editor.xref.groupTitle">
-        <Select
-          className="toolbox__select"
-          onChange={this.changeCase}
-          options={CASES}
-          value={CASES.find(el => el.value === xref.data.get('case')) || CASES[0]}
-        />
+        <label className="toolbox__label">
+          Select language
+          <Select
+            className="toolbox__select"
+            onChange={this.changeLang}
+            options={LANGUAGES}
+            value={language}
+          />
+        </label>
+        <label className="toolbox__label">
+          Select case
+          <Select
+            className="toolbox__select"
+            onChange={this.changeCase}
+            options={CASES[language.value]}
+            value={CASES[language.value].find((el: SelectOption) => el.value === xref.data.get('case')) || CASES[language.value][0]}
+          />
+        </label>
         <Button clickHandler={this.openXrefModal} className="toolbox__button--insert">
           <Icon name="pencil" />
           <Trans i18nKey="Editor.xref.action.change" />
@@ -65,6 +96,11 @@ export default class XrefTools extends React.Component<Props> {
     if (!xref) return null
 
     return xref.type === 'xref' ? xref : null
+  }
+
+  private changeLang = (lng: SelectOption) => {
+    if (lng.value === this.state.language.value) return
+    this.setState({ language: lng })
   }
 
   private changeCase = ({ value }: SelectOption) => {
