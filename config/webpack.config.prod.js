56604777c1ec68ba9b3cdbb49ee1128eaf3edc8b
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-02-27 18:10:01 +0100

    Validate FTL files when building

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -14,6 +14,7 @@ const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const FtlPlugin = require('./FtlPlugin')
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -368,6 +369,9 @@ module.exports = {
       async: false,
       tsconfig: paths.appTsProdConfig,
       tslint: paths.appTsLint,
+    }),
+    new FtlPlugin({
+      input: 'locale/**/*.ftl',
     }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
