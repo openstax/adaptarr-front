a5f230a0a9a017ed0094c40d16cdd35d8e4122ad
Piotr Kozlowski <pior@obrandowani.pl>
2019-03-28 17:02:04 +0100

    Add SET_ROLES reducer

--- src/store/reducers/app.ts
+++ src/store/reducers/app.ts
@@ -1,14 +1,17 @@
 import { AppAction } from 'src/store/actions/app'
-import { SET_LOCALE, SET_AVAILABLE_LOCALES } from 'src/store/constants'
+import { SET_LOCALE, SET_AVAILABLE_LOCALES, SET_ROLES } from 'src/store/constants'
+import Role from 'src/api/role'
 
 export interface State {
   locale: string[],
   availableLocales: string[],
+  roles: Role[],
 }
 
 export const initialState: State = {
   locale: Array.from(navigator.languages),
   availableLocales: [],
+  roles: [],
 }
 
 export function reducer(state: State = initialState, action: AppAction) {
@@ -23,6 +26,12 @@ export function reducer(state: State = initialState, action: AppAction) {
     return {
       ...state,
       availableLocales: action.data,
+    }
+
+  case SET_ROLES:
+    return {
+      ...state,
+      roles: action.data,
     }
 
   default:
