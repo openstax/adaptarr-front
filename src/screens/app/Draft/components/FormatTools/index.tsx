53d5a9daec5bf477eac8c559f72b0d9a1f866a70
Piotr Kozlowski <pior@obrandowani.pl>
2019-05-17 14:20:23 +0200

    Fix format tools

--- src/screens/app/Draft/components/FormatTools/index.tsx
+++ src/screens/app/Draft/components/FormatTools/index.tsx
@@ -31,7 +31,7 @@ export default class FormatTools extends React.Component<Props> {
   render() {
     const { editor, value } = this.props
     const { startBlock } = value
-    const code = startBlock.type === 'code' ? startBlock : null
+    const code = startBlock && startBlock.type === 'code' ? startBlock : null
     
     if (editor.isVoid(startBlock) || code) {
       return null
