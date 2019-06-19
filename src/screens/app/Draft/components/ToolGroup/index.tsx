ef8a94c2b5e7b00ff4ec1da44cfe8dcacd79d070
Piotr Kozlowski <pior@obrandowani.pl>
2019-06-19 10:56:11 +0200

    Allow localizationProps in ToolGroup

--- src/screens/app/Draft/components/ToolGroup/index.tsx
+++ src/screens/app/Draft/components/ToolGroup/index.tsx
@@ -9,16 +9,17 @@ export type Props = {
   title: string,
   toggleState: boolean,
   onToggle: () => any,
+  [localizationProps: string]: any,
 }
 
 export default class ToolGroup extends React.Component<Props> {
   render() {
-    const { children, title, toggleState: open } = this.props
+    const { children, title, toggleState: open, ...args } = this.props
 
     return <div className={"toolgroup" + (open ? ' open' : '')}>
       <div className="toolgroup--header" onClick={this.toggle}>
         <Icon size='small' name={open ? 'arrow-down' : 'arrow-right'} />
-        <Localized id={title}>{title}</Localized>
+        <Localized id={title} {...args}>{title}</Localized>
       </div>
       <div className="toolgroup--content">
         {children}
