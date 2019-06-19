539343001079164a6f10e42799fb6cb135a9076c
Piotr Kozlowski <pior@obrandowani.pl>
2019-06-19 16:14:49 +0200

    Allow custom inlines in SourceElements

--- src/screens/app/Draft/plugins/SourceElements/index.tsx
+++ src/screens/app/Draft/plugins/SourceElements/index.tsx
@@ -1,18 +1,27 @@
 import { Plugin } from 'slate-react'
 
 import { renderBlock, renderInline } from './render'
-import schema from './schema'
+import make_schema, { SchemaOptions } from './schema'
 import onKeyDown from './handlers'
+
+import { SUGGESTION_TYPES } from '../Suggestions/types'
 
 interface CustomPlugin extends Plugin {
   schema: object
 }
 
-const SourceElements: CustomPlugin = {
-  schema,
-  renderBlock,
-  renderInline,
-  onKeyDown,
+const SourceElements = (options: SchemaOptions): CustomPlugin => {
+  const inlines = [
+    ...(options.inlines || []),
+    ...SUGGESTION_TYPES,
+  ]
+
+  return {
+    schema: make_schema({ inlines }),
+    renderBlock,
+    renderInline,
+    onKeyDown,
+  }
 }
 
 export default SourceElements
--- src/screens/app/Draft/plugins/SourceElements/schema.ts
+++ src/screens/app/Draft/plugins/SourceElements/schema.ts
@@ -8,21 +8,24 @@ function normalizeSourceInline(change: Editor, error: SlateError) {
   console.warn('Unhandled source element (inline) violation:', error.code)
 }
 
-export default {
-  blocks: {
-    source_element: {
-      nodes: [{
-        match: { object: 'text' }
-      }],
-      normalize: normalizeSourceBlock,
+export type SchemaOptions = { inlines?: string[] }
+
+export default function make_schema({ inlines = [] }: SchemaOptions) {
+
+  const allowedTypes = inlines.map(type => ({ type }))
+
+  return {
+    blocks: {
+      source_element: {
+        nodes: [{ match: [...allowedTypes, { object: 'text' }] }],
+        normalize: normalizeSourceBlock,
+      },
     },
-  },
-  inlines: {
-    source_element: {
-      nodes: [{
-        match: { object: 'text' }
-      }],
-      normalize: normalizeSourceInline,
+    inlines: {
+      source_element: {
+        nodes: [{ match: [...allowedTypes, { object: 'text' }] }],
+        normalize: normalizeSourceInline,
+      }
     }
   }
 }
