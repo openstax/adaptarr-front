a8d4da92c0850bacc7e7171808a13c3865f690b7
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-06-28 15:05:26 +0200

    Add counter definitions for tables

--- src/screens/app/Draft/plugins/Tables/index.tsx
+++ src/screens/app/Draft/plugins/Tables/index.tsx
@@ -2,12 +2,15 @@ import { Plugin } from 'slate-react'
 
 import renderBlock from './render'
 import schema from './schema'
+import * as queries from './queries'
 
 interface CustomPlugin extends Plugin {
   schema: object
+  queries: { [key: string]: any }
 }
 
 const TablesPlugin: CustomPlugin = {
+  queries,
   schema,
   renderBlock,
 }
