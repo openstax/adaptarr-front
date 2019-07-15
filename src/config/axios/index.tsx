662d623acfadb675cef326e53ffe1d2d418a841b
Piotr Kozlowski <pior@obrandowani.pl>
2019-07-15 11:13:49 +0200

    We do not need to modify axios interceptors

--- src/config/axios/index.tsx
+++ src/config/axios/index.tsx
@@ -4,14 +4,4 @@ const instance = axios.create({
   baseURL: '/api/v1/'
 })
 
-instance.interceptors.request.use((config) => {
-  if (!config.headers) {
-    config.headers = {
-      'Content-Type': 'application/json',
-    }
-  }
-
-  return config
-})
-
 export default instance
