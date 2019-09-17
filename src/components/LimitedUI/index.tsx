48dfbec6ee4f969bd542d33b537c10d627176923
Piotr Kozlowski <pior@obrandowani.pl>
2019-09-17 16:39:22 +0200

    Update LimitedUI component

--- src/components/LimitedUI/index.tsx
+++ src/components/LimitedUI/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 
 import User, { SystemPermission } from 'src/api/user'
-import { TeamPermission } from 'src/api/team'
+import Team, { TeamPermission } from 'src/api/team'
 
 import { IsLoading } from 'src/store/types'
 import { State } from 'src/store/reducers'
@@ -14,8 +14,10 @@ type Props = {
     isLoading: IsLoading
     user: User
   }
-  // User has to have one or more permissions to see hidden UI
-  permissions?: SystemPermission | TeamPermission | (SystemPermission | TeamPermission)[]
+  // User has to have one or more permissions to see hidden UI.
+  permissions: SystemPermission | TeamPermission | (SystemPermission | TeamPermission)[]
+  // If specified then user has to have permissions in this team.
+  team?: Team | number
 }
 
 const mapStateToProps = ({ user }: State) => {
@@ -27,21 +29,42 @@ const mapStateToProps = ({ user }: State) => {
 class LimitedUI extends React.Component<Props> {
 
   public render() {
-    const { user: { user }, permissions = [] } = this.props
+    const { user: { user }, permissions, team } = this.props
 
     // Render component only if user have proper permissions
-    if (typeof permissions === 'string') {
-      if (!user.allPermissions.has(permissions)) {
-        return null
-      }
-    } else {
-      let noAccess = permissions.some(p => {
-        if (!user.allPermissions.has(p)) {
-          return true
+    // or if it is super user.
+    if (!user.is_super) {
+      if (typeof permissions === 'string') {
+        if (team) {
+          const usrTeam = user.teams.find(t => t.id === (typeof team === 'number' ? team : team.id))
+          if (
+            !usrTeam ||
+            !usrTeam.role ||
+            !usrTeam.role.permissions ||
+            !usrTeam.role.permissions.includes(permissions as TeamPermission)
+          ) return null
+        } else if (!user.allPermissions.has(permissions)) {
+          return null
         }
-        return false
-      })
-      if (noAccess) return null
+      } else {
+        if (team) {
+          const usrTeam = user.teams.find(t => t.id === (typeof team === 'number' ? team : team.id))
+          if (
+            !usrTeam ||
+            !usrTeam.role ||
+            !usrTeam.role.permissions ||
+            !permissions.every(p => usrTeam.role!.permissions!.includes(p as TeamPermission))
+          ) return null
+        } else {
+          let noAccess = permissions.some(p => {
+            if (!user.allPermissions.has(p)) {
+              return true
+            }
+            return false
+          })
+          if (noAccess) return null
+        }
+      }
     }
 
     return (
