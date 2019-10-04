2a88c2de31bf89d810aed9dfd60ab5a3431eecff
Piotr Kozlowski <pior@obrandowani.pl>
2019-10-04 18:06:05 +0200

    Allow Labelled component to get texts from ui.ftl

--- src/screens/app/Draft/components/Labelled/index.tsx
+++ src/screens/app/Draft/components/Labelled/index.tsx
@@ -1,25 +1,50 @@
 import * as React from 'react'
-import { InjectedProps, withLocalization } from 'fluent-react/compat'
+import * as PropTypes from 'prop-types'
+import { InjectedProps, withLocalization, ReactLocalization } from 'fluent-react/compat'
 import { RenderBlockProps } from 'slate-react'
 
 export type Props = RenderBlockProps & {
-  className: string,
-  l10nKey: string,
-  args?: object,
+  className: string
+  l10nKey: string
+  // Get text from uiL10n. Default is false and then texts from document.ftl file will be loaded.
+  textFromUI?: boolean
+  args?: object
 }
 
-function Labelled({
-  children, className, attributes, getString, node, l10nKey, args,
-}: Props & InjectedProps) {
-  const message = getString(l10nKey, args)
+class Labelled extends React.Component<Props & InjectedProps> {
+  static contextTypes = {
+    uiL10n: PropTypes.instanceOf(ReactLocalization),
+  }
 
-  return <div
-    className={className}
-    data-label={message}
-    {...attributes}
-    >
-    {children}
-  </div>
+  public render() {
+    const {
+      children,
+      className,
+      attributes,
+      getString,
+      node,
+      l10nKey,
+      textFromUI = false,
+      args,
+    } = this.props
+
+    let message = ''
+    if (textFromUI) {
+      message = this.context.uiL10n.getString(l10nKey, args)
+    } else {
+      message = getString(l10nKey, args)
+    }
+
+    return (
+      <div
+        className={className}
+        data-label={message}
+        {...attributes}
+      >
+        {children}
+      </div>
+    )
+  }
 }
 
 export default withLocalization(Labelled)
