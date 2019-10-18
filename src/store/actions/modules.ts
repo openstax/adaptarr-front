73edcb3b0a9cd93c185014be501297caa98d5168
Piotr Kozlowski <pior@obrandowani.pl>
2019-10-18 18:02:54 +0200

    Introduce ModuleLabels to Redux store

--- src/store/actions/modules.ts
+++ src/store/actions/modules.ts
@@ -1,7 +1,16 @@
 import Module from 'src/api/module'
 
 import * as constants from 'src/store/constants'
-import { ModulesMap, ReferenceTarget } from 'src/store/types'
+import {
+  LabelID,
+  Labels,
+  ModuleID,
+  ModuleLabel,
+  ModuleLabelData,
+  ModulesMap,
+  ModulesWithLabels,
+  ReferenceTarget,
+} from 'src/store/types'
 
 export interface FetchModulesMap {
   (dispatch: any): void
@@ -34,11 +43,62 @@ export interface SetReferenceTargets {
   },
 }
 
+export interface CreateLabel {
+  type: constants.CREATE_LABEL
+  data: ModuleLabelData
+}
+
+export interface RemoveLabel {
+  type: constants.REMOVE_LABEL
+  data: ModuleLabel | LabelID
+}
+
+export interface SetLabels {
+  type: constants.SET_LABELS
+  data: Labels
+}
+
+export interface UpdateLabel {
+  type: constants.UPDATE_LABEL
+  data: {
+    id: LabelID
+    data: ModuleLabelData
+  }
+}
+
+export interface SetModulesWithLabels {
+  type: constants.SET_MODULES_WITH_LABELS
+  data: ModulesWithLabels
+}
+
+export interface AddLabelToModule {
+  type: constants.ADD_LABEL_TO_MODULE
+  data: {
+    module: ModuleID
+    label: ModuleLabel | LabelID
+  }
+}
+
+export interface RemoveLabelFromModule {
+  type: constants.REMOVE_LABEL_FROM_MODULE
+  data: {
+    module: ModuleID
+    label: ModuleLabel | LabelID
+  }
+}
+
 export type ModulesAction
   = SetModulesMap
   | AddModuleToMap
   | RemoveModuleFromMap
   | SetReferenceTargets
+  | SetLabels
+  | CreateLabel
+  | RemoveLabel
+  | UpdateLabel
+  | SetModulesWithLabels
+  | AddLabelToModule
+  | RemoveLabelFromModule
 
 const setModulesMap = (payload: ModulesMap): SetModulesMap => ({
   type: constants.SET_MODULES_MAP,
@@ -51,6 +111,10 @@ export const fetchModulesMap = (): FetchModulesMap => (dispatch: React.Dispatch<
       dispatch(setModulesMap(new Map(
         modules.map((mod: Module): [string, Module] => [mod.id, mod]))
       ))
+      const labels: Labels = JSON.parse(localStorage.getItem('labels') || '{}')
+      const modulesWithLabels = JSON.parse(localStorage.getItem('modulesWithLabels') || '{}')
+      dispatch(setLabels(labels))
+      dispatch(setModulesWithLabels(modulesWithLabels))
     })
     .catch(e => {
       throw new Error(e.message)
@@ -114,3 +178,53 @@ export const fetchReferenceTargets = (forModule: Module) => async (dispatch: any
 
   dispatch(setReferenceTargets(forModule.id, Array.from(targets.values())))
 }
+
+export const setLabels = (labels: Labels): SetLabels => ({
+  type: constants.SET_LABELS,
+  data: labels,
+})
+
+export const createLabel = (label: ModuleLabelData): CreateLabel => ({
+  type: constants.CREATE_LABEL,
+  data: label,
+})
+
+export const removeLabel = (label: ModuleLabel | LabelID): RemoveLabel => ({
+  type: constants.REMOVE_LABEL,
+  data: label,
+})
+
+export const updateLabel = (id: LabelID, data: ModuleLabelData): UpdateLabel => ({
+  type: constants.UPDATE_LABEL,
+  data: {
+    id,
+    data,
+  },
+})
+
+export const setModulesWithLabels = (mwl: ModulesWithLabels): SetModulesWithLabels => ({
+  type: constants.SET_MODULES_WITH_LABELS,
+  data: mwl,
+})
+
+export const addLabelToModule = (
+  module: ModuleID,
+  label: ModuleLabel | LabelID
+): AddLabelToModule => ({
+  type: constants.ADD_LABEL_TO_MODULE,
+  data: {
+    module,
+    label,
+  },
+})
+
+export const removeLabelFromModule = (
+  module: ModuleID,
+  label: ModuleLabel | LabelID
+): RemoveLabelFromModule => ({
+  type: constants.REMOVE_LABEL_FROM_MODULE,
+  data: {
+    module,
+    label,
+  },
+})
